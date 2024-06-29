e03297d3cadc3e3577e532034cd6724600655b29
Yoink3000 <hugongai98@gmail.com>
2024-06-29 14:46:33 +0800

    fix setup script

--- assets/build/setup.sh
+++ assets/build/setup.sh
@@ -6,11 +6,6 @@ apt update -y > /dev/null
 echo Upgrading apt...
 apt upgrade -y > /dev/null
 
-echo Setting up for cloudflared
-mkdir -p --mode=0755 /usr/share/keyrings
-curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflared.list
-
 echo Installing apt packages...
 apt install -y git \
     python3.10 \
@@ -21,6 +16,14 @@ apt install -y git \
     wget \
     aria2 \
     cloudflared > /dev/null
+
+echo Setting up for cloudflared
+mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | tee /usr/share/keyrings/cloudflare-main.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflared.list
+
+echo Installing cloudflared...
+apt install -y cloudflared > /dev/null
 
 echo Installing necessary python packages...
 pip install jupyterlab "huggingface_hub[hf_transfer]" humanize > /dev/null
