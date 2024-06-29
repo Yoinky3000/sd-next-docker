fb7d2f94ba52d4b40cdeb28d32e3b5527ac4f9d4
Yoink3000 <hugongai98@gmail.com>
2024-06-29 14:56:38 +0800

    fix setup script

--- assets/build/setup.sh
+++ assets/build/setup.sh
@@ -15,15 +15,16 @@ apt install -y git \
     curl \
     wget \
     aria2 \
-    lsb-release > /dev/null
+    lsb-release \
+    sudo > /dev/null
 
 echo Setting up for cloudflared
-mkdir -p --mode=0755 /usr/share/keyrings
-curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | tee /usr/share/keyrings/cloudflare-main.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflared.list
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflared.list
 
 echo Installing cloudflared...
-apt install -y cloudflared > /dev/null
+sudo apt install -y cloudflared > /dev/null
 
 echo Installing necessary python packages...
 pip install jupyterlab "huggingface_hub[hf_transfer]" humanize > /dev/null
