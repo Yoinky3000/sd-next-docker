b6f0c7d39e6ca072c796a1cae944d6c196bdc7fc
Yoink3000 <hugongai98@gmail.com>
2024-06-29 14:42:12 +0800

    integrate cloudflared for faster access

--- assets/build/setup.sh
+++ assets/build/setup.sh
@@ -6,6 +6,11 @@ apt update -y > /dev/null
 echo Upgrading apt...
 apt upgrade -y > /dev/null
 
+echo Setting up for cloudflared
+mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
 echo Installing apt packages...
 apt install -y git \
     python3.10 \
@@ -14,7 +19,8 @@ apt install -y git \
     python3-pip \
     curl \
     wget \
-    aria2 > /dev/null
+    aria2 \
+    cloudflared > /dev/null
 
 echo Installing necessary python packages...
 pip install jupyterlab "huggingface_hub[hf_transfer]" humanize > /dev/null
--- assets/scripts/sdnext-start.sh
+++ assets/scripts/sdnext-start.sh
@@ -12,5 +12,13 @@ TERM=dumb TORCH_COMMAND=$TORCH_COMMAND ./webui.sh --port 3001 2>/dev/null | whil
     echo "$line" >> /workspace/logs/sdnext.log
     if [[ $line == *"localhost"* ]]; then
         echo "SD Next is ready"
+        echo "Starting Cloudflared..."
+        script -q -c "cloudflared tunnel --url http://localhost:3001" /dev/null | while read -r line; do
+            url=$(echo "$line" | grep -Eo 'https://[^ >]+trycloudflare.com' | head -1)
+            if [ -n "$url" ]; then
+                echo "Cloudflared is ready, visit $url to access SD Next"
+                break
+            fi
+        done
     fi
 done
